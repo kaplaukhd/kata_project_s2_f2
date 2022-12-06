a2485f076b1443115a6f5075c082030bc987ca37
kaplaukhd <kaplaukhd@gmail.com>
2022-12-06 10:55:19 +0500

    complete

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -8,61 +8,61 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-
-
     public UserDaoJDBCImpl() {
 
     }
-     private static final Connection connection = Util.connection();
 
     public void createUsersTable() {
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS kata_f2_s1.user" +
-                    "(id int not null auto_increment," +
-                    " name VARCHAR(50), " +
-                    "lastname VARCHAR(50), " +
-                    "age int, " +
-                    "PRIMARY KEY (id));");
+        try (Connection connection = Util.connection()) {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS kata_f2_s1.user" + "(id int not null auto_increment," + " name VARCHAR(50), " + "lastname VARCHAR(50), " + "age int, " + "PRIMARY KEY (id));");
+            connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public void dropUsersTable() {
-        try (Statement statement = connection.createStatement()) {
+        try (Connection connection = Util.connection()) {
+            Statement statement = connection.createStatement();
             statement.executeUpdate("Drop table if exists kata_f2_s1.user;");
+            connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try (PreparedStatement statement = connection.prepareStatement("INSERT INTO kata_f2_s1.user(name, lastname, age) VALUES(?,?,?)")) {
+        try (Connection connection = Util.connection()) {
+            PreparedStatement statement = connection
+                    .prepareStatement("INSERT INTO kata_f2_s1.user(name, lastname, age) VALUES(?,?,?)");
             statement.setString(1, name);
             statement.setString(2, lastName);
             statement.setByte(3, age);
             statement.executeUpdate();
-            System.out.println("User с именем " + name + " добавлен в базу данных");
+            connection.commit();
         } catch (SQLException e) {
-            try {
-                connection.rollback();
-            } catch (SQLException exception) {
-                exception.printStackTrace();
-            }
+            System.out.println("Can't save user " + name);
+            e.printStackTrace();
         }
+        System.out.println("User с именем " + name + " добавлен в базу данных");
     }
 
     public void removeUserById(long id) {
-        try (Statement statement = connection.createStatement()) {
+        try (Connection connection = Util.connection()) {
+            Statement statement = connection.createStatement();
             statement.executeUpdate("DELETE FROM kata_f2_s1.user WHERE id;");
+            connection.commit();
         } catch (SQLException e) {
+            System.out.println("User with id: " + id + "could not deleted");
             e.printStackTrace();
         }
     }
 
     public List<User> getAllUsers() {
         List<User> userList = new ArrayList<>();
-        try (Statement statement = connection.createStatement()) {
+        try (Connection connection = Util.connection()) {
+            Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT id, name, lastName, age from kata_f2_s1.user");
             while (resultSet.next()) {
                 User tempUser = new User();
@@ -72,6 +72,7 @@ public class UserDaoJDBCImpl implements UserDao {
                 tempUser.setAge((byte) resultSet.getInt("age"));
                 userList.add(tempUser);
             }
+            connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -79,8 +80,10 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-        try (Statement statement = connection.createStatement()) {
+        try (Connection connection = Util.connection()) {
+            Statement statement = connection.createStatement();
             statement.executeUpdate("TRUNCATE TABLE kata_f2_s1.user");
+            connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
         }
